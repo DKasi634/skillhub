942902a9952e0a965009e8c3c5253545a94c1178
Daniel <dkasi634@gmail.com>
2025-03-25 01:24:50 +0300

    Updated email redirect link in supabase utils

--- src/utils/supabase/supabase.auth.ts
+++ src/utils/supabase/supabase.auth.ts
@@ -12,7 +12,7 @@ export const supabaseSignUp = async (
   );
   try {
     const { data, error } = await supabase.auth.signUp({ email, password, options:{
-      emailRedirectTo:`${window.location.origin}/callback`
+      emailRedirectTo:`https://skillhub-web.netlify.app/callback`
     } });
 
     if (error) {
